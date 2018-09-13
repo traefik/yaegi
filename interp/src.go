9cfbcebf86045fd7195473273a5958edb2cd9c4e
Marc Vertes <mvertes@free.fr>
2018-09-13 17:02:22 +0200

    src: execute entry point and init functions after all package files are parsed.

--- interp/src.go
+++ interp/src.go
@@ -10,10 +10,16 @@ import (
 
 func (interp *Interpreter) importSrcFile(path string) {
 	dir := pkgDir(path)
+
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	initNodes := []*Node{}
+	rootNodes := []*Node{}
+
+	// Parse source files
 	for _, file := range files {
 		name := file.Name()
 		if len(name) <= 3 || name[len(name)-3:] != ".go" {
@@ -22,27 +28,53 @@ func (interp *Interpreter) importSrcFile(path string) {
 		if len(name) > 8 && name[len(name)-8:] == "_test.go" {
 			continue
 		}
+
 		//log.Println("src", name)
 		buf, err := ioutil.ReadFile(filepath.Join(dir, name))
 		if err != nil {
 			log.Fatal(err)
 		}
-		interp.Eval(string(buf))
+
+		_, root := interp.Ast(string(buf))
+		initNodes = append(initNodes, interp.Cfg(root)...)
+		rootNodes = append(rootNodes, root)
+		if interp.AstDot {
+			root.AstDot(DotX())
+		}
+	}
+
+	if interp.NoRun {
+		return
+	}
+
+	// Once all package sources have been parsed, execute entry points then init functions
+	for _, n := range rootNodes {
+		interp.fsize++
+		interp.resizeFrame()
+		runCfg(n.start, interp.Frame)
+	}
+
+	for _, n := range initNodes {
+		Run(n, interp.Frame, nil, nil, nil, nil, true, false)
 	}
 }
 
+// pkgDir returns the abolute path in filesystem for a package given its name
 func pkgDir(path string) string {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	dir = filepath.Join(dir, "vendor", path)
 	if _, err := os.Stat(dir); err == nil {
 		return dir
 	}
+
 	dir = filepath.Join(build.Default.GOPATH, "src", path)
 	if _, err := os.Stat(dir); err != nil {
 		log.Fatal(err)
 	}
+
 	return dir
 }
