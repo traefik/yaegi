1e3443b3be3218110245deb5ec948ce1ede30cab
Fernandez Ludovic <ludovic@containo.us>
2019-01-23 23:09:53 +0100

    refactor: remove obsolete tests for plugins.

--- _test/src1.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/containous/provider2"
-
-func main() {
-	provider2.Sample()
-}
--- vendor/github.com/containous/provider2/provider.go
+++
@@ -1,8 +0,0 @@
-package provider2
-
-import "fmt"
-
-// Sample is a dummy test function
-func Sample() {
-	fmt.Println("Hello from Provider2")
-}
