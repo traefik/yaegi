294a1d25814f9b9c0ead596f90fb49c8f703adec
Marc Vertes <mvertes@free.fr>
2019-02-03 14:37:59 +0100

    test: add comment to exported function

--- example/closure/src/foo/bar/bar.go
+++ example/closure/src/foo/bar/bar.go
@@ -6,6 +6,7 @@ import (
 
 var version = "v1"
 
+// NewSample returns a closure generator, which itself returns a func(string)
 func NewSample() func(string, string) func(string) {
 	fmt.Println("in NewSample")
 	return func(val string, name string) func(string) {
