1ccc36a690149c864ba64887d4500ee3e6bb601a
Marc Vertes <mvertes@free.fr>
2019-03-16 18:36:44 +0100

    feat: implement support of 'for x := range channel' expression (#119)

---
+++ _test/chan7.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	queue := make(chan string, 2)
+	queue <- "one"
+	queue <- "two"
+	close(queue)
+	for elem := range queue {
+		fmt.Println(elem)
+	}
+}
+
+// Output:
+// one
+// two
