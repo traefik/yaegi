d281b0e7c4f91f15ade95d0816b0072963a5e38e
Marc Vertes <mvertes@free.fr>
2019-04-02 13:34:58 +0200

    fix: wrong returned type in define from function call in global context
    This is a regression since the recent changes in type and memory management.
    Now the returned type for a function call is computed in nodeType()
    instead of getReturnedType() (deleted), fixing definition in global
    context (triggered in gta).
    
    Fix #147

--- example/inception/inception.go
+++ example/inception/inception.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	log.SetFlags(log.Lshortfile)
 	i := interp.New(interp.Opt{})
 	i.Use(interp.ExportValue)
 	if _, err := i.Eval(`import "github.com/containous/yaegi/interp"`); err != nil {
