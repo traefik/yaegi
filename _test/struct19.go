686769fa84e22d0aee54eb6b0a0f0658c2c4096f
Marc Vertes <mvertes@free.fr>
2019-06-12 18:52:41 +0200

    fix: add support for tags in struct fields (#220)

---
+++ _test/struct19.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Config struct {
+	Users        `json:"users,omitempty" mapstructure:","`
+	UsersFile    string `json:"usersFile,omitempty"`
+	Realm        string `json:"realm,omitempty"`
+	RemoveHeader bool   `json:"removeHeader,omitempty"`
+	HeaderField  string `json:"headerField,omitempty" export:"true"`
+}
+
+// Users holds a list of users
+type Users []string
+
+func CreateConfig() *Config {
+	return &Config{}
+}
+
+func main() {
+	c := CreateConfig()
+	fmt.Println(c)
+}
