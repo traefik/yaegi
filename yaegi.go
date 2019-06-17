430269e6ddd63486056d89f2495fe1349b9b0af3
Marc Vertes <mvertes@free.fr>
2019-06-17 18:08:48 +0200

    feature: isolate stdlib "unsafe" package
    Store "unsafe" package in its own separate package instead of being
    provided by default in stdlib values. To bind unsafe in interpreter,
    one must now use:
    
    import "github.com/containous/yaegi/stdlib/unsafe"
    
    and later:
    
    interpreter.Use(unsafe.Value)
    
    By default, there is now no direct or indirect dependence on "unsafe" in
    yaegi or stdlib package.

--- yaegi.go
+++ yaegi.go
@@ -4,6 +4,7 @@ package main
 //go:generate go generate github.com/containous/yaegi/cmd/goexports
 //go:generate go generate github.com/containous/yaegi/stdlib
 //go:generate go generate github.com/containous/yaegi/stdlib/syscall
+//go:generate go generate github.com/containous/yaegi/stdlib/unsafe
 
 import (
 	"flag"
