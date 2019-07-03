017ea5a42286fbe07e75a9a40f8c9a0492918894
Fernandez Ludovic <ludovic@containo.us>
2019-07-03 14:28:05 +0200

    chore: move yaegi command.

--- cmd/yaegi/yaegi.go
+++ cmd/yaegi/yaegi.go
@@ -1,11 +1,5 @@
 package main
 
-//go:generate go generate github.com/containous/yaegi/interp
-//go:generate go generate github.com/containous/yaegi/cmd/goexports
-//go:generate go generate github.com/containous/yaegi/stdlib
-//go:generate go generate github.com/containous/yaegi/stdlib/syscall
-//go:generate go generate github.com/containous/yaegi/stdlib/unsafe
-
 import (
 	"flag"
 	"fmt"
