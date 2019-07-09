eaf8e92fc40e9a47cc6578974f6ed2840ecd21f1
Marc Vertes <mvertes@free.fr>
2019-07-09 16:04:53 +0200

    feature: set default GOPATH for command line and allow self interpreting
    To be on par with "go run", the default GOPATH is applied to yaegi
    command line (note that interp default behaviour is unchanged).
    
    Flag state is explicitely initialized prior to start the REPL, to
    allow recursion.
    
    Make sure that stdlib symbols define themselves to allow recursion.
    
    It is now possible to perform the following:
    
    ```console
    $ go build cmd/yaegi/yaegi.go
    $ ./yaegi cmd/yaegi/yaegi.go cmd/yaegi/yaegi.go cmd/yaegi/yaegi.go
    >
    ```

--- stdlib/syscall/syscall.go
+++ stdlib/syscall/syscall.go
@@ -5,4 +5,10 @@ import "reflect"
 // Symbols stores the map of syscall package symbols
 var Symbols = map[string]map[string]reflect.Value{}
 
+func init() {
+	Symbols["github.com/containous/yaegi/stdlib/syscall"] = map[string]reflect.Value{
+		"Symbols": reflect.ValueOf(Symbols),
+	}
+}
+
 //go:generate ../../cmd/goexports/goexports syscall
--- stdlib/unsafe/unsafe.go
+++ stdlib/unsafe/unsafe.go
@@ -5,4 +5,10 @@ import "reflect"
 // Symbols stores the map of unsafe package symbols
 var Symbols = map[string]map[string]reflect.Value{}
 
+func init() {
+	Symbols["github.com/containous/yaegi/stdlib/unsafe"] = map[string]reflect.Value{
+		"Symbols": reflect.ValueOf(Symbols),
+	}
+}
+
 //go:generate ../../cmd/goexports/goexports unsafe
