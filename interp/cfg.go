c2e4f16088915f35e573186d1f2fa41132cbdc38
Marc Vertes <mvertes@free.fr>
2019-07-11 13:29:38 +0200

    style: replace if / if else by switch / case (lint)

--- interp/cfg.go
+++ interp/cfg.go
@@ -750,11 +750,13 @@ func (interp *Interpreter) cfg(root *node) ([]*node, error) {
 
 		case caseBody:
 			wireChild(n)
-			if typeSwichAssign(n) && len(n.child) > 1 {
+			switch {
+			case typeSwichAssign(n) && len(n.child) > 1:
 				n.start = n.child[1].start
-			} else if len(n.child) == 0 {
+			case len(n.child) == 0:
+				// empty case body: jump to switch node (exit node)
 				n.start = n.anc.anc.anc
-			} else {
+			default:
 				n.start = n.child[0].start
 			}
 
