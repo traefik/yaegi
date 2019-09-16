56038b26ba56069134701aec5ebe65df5757fd5c
Marc Vertes <mvertes@free.fr>
2019-09-16 13:45:27 +0200

    feature: additional binary packages in stdlib
    Generate wrappers for the following packages:
    crupto/ed25519 (go1_13 only)
    debug/dwarf
    debug/elf
    debug/gosym
    debug/pe
    debug/plan9obj
    net/http/pprof
    runtime/pprof
    runtime/trace
    
    Goexports prints a warning when a package is not found (i.e
    crypto/25519 for go1.11 and go1.12) but does not fail.

--- stdlib/stdlib.go
+++ stdlib/stdlib.go
@@ -19,11 +19,12 @@ func init() {
 //go:generate ../cmd/goexports/goexports bufio bytes
 //go:generate ../cmd/goexports/goexports compress/bzip2 compress/flate compress/gzip compress/lzw compress/zlib
 //go:generate ../cmd/goexports/goexports container/heap container/list container/ring
-//go:generate ../cmd/goexports/goexports context crypto crypto/aes crypto/cipher crypto/des crypto/dsa
-//go:generate ../cmd/goexports/goexports crypto/ecdsa crypto/elliptic crypto/hmac crypto/md5 crypto/rand
+//go:generate ../cmd/goexports/goexports context crypto crypto/aes crypto/cipher crypto/des crypto/dsa crypto/ecdsa
+//go:generate ../cmd/goexports/goexports crypto/ed25519 crypto/elliptic crypto/hmac crypto/md5 crypto/rand
 //go:generate ../cmd/goexports/goexports crypto/rc4 crypto/rsa crypto/sha1 crypto/sha256 crypto/sha512
 //go:generate ../cmd/goexports/goexports crypto/subtle crypto/tls crypto/x509 crypto/x509/pkix
 //go:generate ../cmd/goexports/goexports database/sql database/sql/driver
+//go:generate ../cmd/goexports/goexports debug/dwarf debug/elf debug/gosym debug/macho debug/pe debug/plan9obj
 //go:generate ../cmd/goexports/goexports encoding encoding/ascii85 encoding/asn1 encoding/base32
 //go:generate ../cmd/goexports/goexports encoding/base64 encoding/binary encoding/csv encoding/gob
 //go:generate ../cmd/goexports/goexports encoding/hex encoding/json encoding/pem encoding/xml
@@ -38,11 +39,11 @@ func init() {
 //go:generate ../cmd/goexports/goexports math math/big math/bits math/cmplx math/rand
 //go:generate ../cmd/goexports/goexports mime mime/multipart mime/quotedprintable
 //go:generate ../cmd/goexports/goexports net net/http net/http/cgi net/http/cookiejar net/http/fcgi
-//go:generate ../cmd/goexports/goexports net/http/httptest net/http/httptrace net/http/httputil
+//go:generate ../cmd/goexports/goexports net/http/httptest net/http/httptrace net/http/httputil net/http/pprof
 //go:generate ../cmd/goexports/goexports net/mail net/rpc net/rpc/jsonrpc net/smtp net/textproto net/url
 //go:generate ../cmd/goexports/goexports os os/exec os/signal os/user
 //go:generate ../cmd/goexports/goexports path path/filepath reflect regexp regexp/syntax
-//go:generate ../cmd/goexports/goexports runtime runtime/debug
+//go:generate ../cmd/goexports/goexports runtime runtime/debug runtime/pprof runtime/trace
 //go:generate ../cmd/goexports/goexports sort strconv strings sync sync/atomic
 //go:generate ../cmd/goexports/goexports text/scanner text/tabwriter text/template text/template/parse
 //go:generate ../cmd/goexports/goexports time unicode unicode/utf16 unicode/utf8
