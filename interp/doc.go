fc3987be6a069739a4ddb60a6c385e09388d62d4
Marc Vertes <mvertes@free.fr>
2019-10-12 14:49:43 +0200

    fix: assign binary func to func type var
    Only interpreted func could be assigned to func type variables.
    Call() can now invoke binary func directly.
    
    Fixes #408

--- interp/doc.go
+++ interp/doc.go
@@ -1,5 +1,5 @@
 /*
-Package interp provides a complete Go interpreter
+Package interp provides a complete Go interpreter.
 
 For the Go language itself, refer to the official Go specification
 https://golang.org/ref/spec.
@@ -7,4 +7,4 @@ https://golang.org/ref/spec.
 */
 package interp
 
-// BUG(marc): Type checking is not implemented yet
+// BUG(marc): Type checking is not implemented yet.
