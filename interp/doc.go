da5a61093df78eba894af8c1c34c5927556981fe
Marc Vertes <mvertes@free.fr>
2019-12-20 23:15:46 +0100

    doc: document import of source and binary packages
    Fixes #469

--- interp/doc.go
+++ interp/doc.go
@@ -4,6 +4,19 @@ Package interp provides a complete Go interpreter.
 For the Go language itself, refer to the official Go specification
 https://golang.org/ref/spec.
 
+Importing packages
+
+Packages can be imported in source or binary form, using the standard
+Go import statement. In source form, packages are searched in GOPATH
+(Go modules are not supported yet). Packages can be installed manually
+with the following command:
+
+	GO111MODULE=off go get -d <package_path>
+
+Binary form packages are compiled and linked with the interpreter
+executable, and exposed to scripts with the Use method. The goexports
+command can be used to generate package wrappers.
+
 Custom build tags
 
 Custom build tags allow to control which files in imported source
