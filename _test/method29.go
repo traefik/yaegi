f44d7b67f3ae23699eee8305b251d3090da4d5b7
Marc Vertes <mvertes@free.fr>
2020-01-23 11:33:46 +0100

    test: robustify _test/method29.go for CI

--- _test/method29.go
+++ _test/method29.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"net"
 )
 
 var lookupHost = net.DefaultResolver.LookupHost
 
 func main() {
-	fmt.Println(lookupHost(context.Background(), "localhost"))
+	res, err := lookupHost(context.Background(), "localhost")
+	println(len(res) > 0, err == nil)
 }
 
 // Output:
-// [::1 127.0.0.1] <nil>
+// true true
