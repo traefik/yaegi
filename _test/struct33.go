d3ad1cabb07c4571a9e88e7cf728dc8ee87052f9
Marc Vertes <mvertes@free.fr>
2020-03-03 17:43:33 +0100

    test: simplify

--- _test/struct33.go
+++ _test/struct33.go
@@ -13,15 +13,6 @@ type T1 struct {
 
 func (t T0) F1() bool { println("in F1"); return true }
 
-type T2 struct {
-	t1 T1
-}
-
-func (t2 *T2) f() {
-	info := t2.t1.info("foo")
-	println(info.F1())
-}
-
 var t0 = T0{"t0"}
 
 func look(s string) T0 { println("in look"); return t0 }
