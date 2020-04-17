56925e6fea5303333a2872e68f51f68b13a75ec6
Marc Vertes <mvertes@free.fr>
2020-04-17 14:40:56 +0200

    fix: correct branch control flow graph for parenthesis expressions (#583)

---
+++ _test/not0.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+        a := 0
+        b := true
+	c := false
+        if (b && c) {
+                a = 1
+        } else {
+                a = -1
+        }
+        println(a)
+}
+
+// Output:
+// -1
---
+++ _test/not1.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+        a := 0
+        b := true
+        if (!b) {
+                a = 1
+        } else {
+                a = -1
+        }
+        println(a)
+}
+
+// Output:
+// -1
