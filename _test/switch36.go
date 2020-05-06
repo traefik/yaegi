5a9989bad0c78f88c088be5b8df0f9bfbf6b8b8f
Marc Vertes <mvertes@free.fr>
2020-05-06 15:31:37 +0200

    fix: hande empty clause in switch if statement
    The CFG was wrong in presence of an empty last clause.
    
    Fixes #634.

---
+++ _test/switch36.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	a := 2
+	switch {
+	case a == 1:
+		println(1)
+	case a == 2:
+	}
+	println("bye")
+}
+
+// Output:
+// bye
---
+++ _test/switch37.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	a := 2
+	switch {
+	case a == 1:
+		println(1)
+	case a == 3:
+	default:
+	}
+	println("bye")
+}
+
+// Output:
+// bye
