80a77385400bdccaedf266c610a45fddf303afe9
Nicholas Wiersma <nick@wiersma.co.za>
2020-05-27 09:43:01 +0200

    Fix import source symbol package

---
+++ _test/baz-bat/baz-bat.go
@@ -0,0 +1,3 @@
+package baz
+
+var Name = "baz-bat"
---
+++ _test/import9.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/containous/yaegi/_test/baz-bat"
+
+func main() {
+	println(baz.Name)
+}
+
+// Output:
+// baz-bat
