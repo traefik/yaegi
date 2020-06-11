7db7530b3208956a0cc8c9a58cd3b4767621e4e8
Nicholas Wiersma <nick@wiersma.co.za>
2020-06-11 09:43:30 +0200

    add/fix tests

---
+++ _test/const13.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const tooBig = 1267650600228229401496703205376
+	const huge = 1 << 100
+	const large = huge >> 38
+
+	fmt.Println(large)
+}
+
+// Output:
+// 4611686018427387904
