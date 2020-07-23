e5a7b0de11385acaa3c73a030f950e52d09297e4
mpl <mathieu.lonjaret@gmail.com>
2020-07-23 10:58:33 +0200

    extract: new package to extract symbols from a dependency
    cmd/goexports is now based on it.
    
    Updates #639 
    
    Co-authored-by: Marc Vertes <mvertes@free.fr>

---
+++ extract/testdata/4/src/guthib.com/bar/vendor/guthib.com/baz/go.mod
@@ -0,0 +1,3 @@
+module guthib.com/baz
+
+go 1.14
---
+++ stdlib/stdlib-maphash.go
@@ -0,0 +1,6 @@
+// +build go1.14,!go1.15
+
+// Package stdlib provides wrappers of standard library packages to be imported natively in Yaegi.
+package stdlib
+
+//go:generate ../cmd/goexports/goexports hash/maphash
