68911f8b4e5c92c379a61b8f5b506a30e3197067
Marc Vertes <mvertes@free.fr>
2020-07-23 11:51:53 +0200

    fix: type assertion expression was not forwarding type
    The detection of special cases of interpreter functions and interfaces
    is more precise. It allows at least to complete parsing of
    code where type is derived from a type assertion expression.
    
    Fixes #770.

--- interp/type.go
+++ interp/type.go
@@ -1536,11 +1536,13 @@ func chanElement(t *itype) *itype {
 
 func isBool(t *itype) bool { return t.TypeOf().Kind() == reflect.Bool }
 func isChan(t *itype) bool { return t.TypeOf().Kind() == reflect.Chan }
+func isFunc(t *itype) bool { return t.TypeOf().Kind() == reflect.Func }
+func isMap(t *itype) bool  { return t.TypeOf().Kind() == reflect.Map }
 func isSendChan(t *itype) bool {
 	rt := t.TypeOf()
 	return rt.Kind() == reflect.Chan && rt.ChanDir() == reflect.SendDir
 }
-func isMap(t *itype) bool { return t.TypeOf().Kind() == reflect.Map }
+
 func isArray(t *itype) bool {
 	k := t.TypeOf().Kind()
 	return k == reflect.Array || k == reflect.Slice
