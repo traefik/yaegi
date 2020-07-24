traefik/yaegi

commit 9c51f6bb694888db4cebc2886680d41b98552105

fix: correct range on arrays of interface objects

interp/value.go
@@ -155,17 +155,36 @@ func genValueArray(n *node) func(*frame) reflect.Value {
 
 func genValueRangeArray(n *node) func(*frame) reflect.Value {
 	value := genValue(n)
-	// dereference array pointer, to support array operations on array pointer
-	if n.typ.TypeOf().Kind() == reflect.Ptr {
+
+	switch {
+	case n.typ.TypeOf().Kind() == reflect.Ptr:
+		// dereference array pointer, to support array operations on array pointer
 		return func(f *frame) reflect.Value {
 			return value(f).Elem()
 		}
-	}
-	return func(f *frame) reflect.Value {
-		// This is necessary to prevent changes in the returned
-		// reflect.Value being reflected back to the value used
-		// for the range expression.
-		return reflect.ValueOf(value(f).Interface())
+	case n.typ.val.cat == interfaceT:
+		return func(f *frame) reflect.Value {
+			val := value(f)
+			v := []valueInterface{}
+			for i := 0; i < val.Len(); i++ {
+				switch av := val.Index(i).Interface().(type) {
+				case []valueInterface:
+					v = append(v, av...)
+				case valueInterface:
+					v = append(v, av)
+				default:
+					panic(n.cfgErrorf("invalid type %v", val.Index(i).Type()))
+				}
+			}
+			return reflect.ValueOf(v)
+		}
+	default:
+		return func(f *frame) reflect.Value {
+			// This is necessary to prevent changes in the returned
+			// reflect.Value being reflected back to the value used
+			// for the range expression.
+			return reflect.ValueOf(value(f).Interface())
+		}
 	}
 }
 

_test/issue-784.go
@@ -1,0 +1,39 @@
+package main
+
+import "fmt"
+
+// Filter is a filter interface
+type Filter interface {
+	Bounds(srcBounds string) (dstBounds string)
+}
+
+// GIFT type
+type GIFT struct {
+	Filters []Filter
+}
+
+// New creates a new filter list and initializes it with the given slice of filters.
+func New(filters ...Filter) *GIFT {
+	return &GIFT{
+		Filters: filters,
+	}
+}
+
+// Bounds calculates the appropriate bounds for the result image after applying all the added filters.
+func (g *GIFT) Bounds(srcBounds string) (dstBounds string) {
+	dstBounds = srcBounds
+	for _, f := range g.Filters {
+		dstBounds = f.Bounds(dstBounds)
+	}
+	return dstBounds
+}
+
+func main() {
+	var filters []Filter
+	bounds := "foo"
+	g := New(filters...)
+	fmt.Println(g.Bounds(bounds))
+}
+
+// Output:
+// foo

commit 0f8f99169985b5654bd333cab7aeadbe91fe8df4

fix: regression on range following #787
This change fixes a nil pointer dereference regression introduced by
PR #787 which caused a traefik plugin to fail.

interp/cfg.go
@@ -602,7 +602,9 @@ func (interp *Interpreter) cfg(root *node, pkgID string) ([]*node, error) {
 					}
 					n.gen = nop
 					n.findex = -1
-					sc.sym[dest.ident].kind = constSym
+					if sym, _, ok := sc.lookup(dest.ident); ok {
+						sym.kind = constSym
+					}
 					if childPos(n) == len(n.anc.child)-1 {
 						sc.iota = 0
 					} else {

interp/value.go
@@ -162,7 +162,7 @@ func genValueRangeArray(n *node) func(*frame) reflect.Value {
 		return func(f *frame) reflect.Value {
 			return value(f).Elem()
 		}
-	case n.typ.val.cat == interfaceT:
+	case n.typ.val != nil && n.typ.val.cat == interfaceT:
 		return func(f *frame) reflect.Value {
 			val := value(f)
 			v := []valueInterface{}