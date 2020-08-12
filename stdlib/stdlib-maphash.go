1fe75f149d3d9565eb8123816555bc7716f1e8fa
Ludovic Fernandez <ldez@users.noreply.github.com>
2020-08-12 12:38:04 +0200

    feat: update stdlib mapping for go1.15
    - drop stdlib for go1.13
    - generate stdlib for go1.15
    -  update CI configuration.

--- stdlib/stdlib-maphash.go
+++
@@ -1,6 +0,0 @@
-// +build go1.14,!go1.15
-
-// Package stdlib provides wrappers of standard library packages to be imported natively in Yaegi.
-package stdlib
-
-//go:generate ../cmd/goexports/goexports hash/maphash
