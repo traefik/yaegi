3d3ea3ee0a378002056d497e3378c5b564ebecdc
Marc Vertes <mvertes@free.fr>
2020-10-13 13:40:57 +0200

    fix: simplify test

--- _test/issue-880.go
+++ _test/issue-880.go
@@ -10,10 +10,6 @@ func main() {
 	var buf2 []byte
 	buf1 = []byte("Hallo\nTest\nLine3")
 
-	// works
-	buf2 = append(buf2, append(buf1, []byte("Line4\n")...)...)
-
-	// does not work
 	s := bufio.NewScanner(bytes.NewReader(buf1))
 	for s.Scan() {
 		buf2 = append(buf2, append(s.Bytes(), []byte("\n")...)...)
@@ -22,9 +18,6 @@ func main() {
 }
 
 // Output:
-// Hallo
-// Test
-// Line3Line4
 // Hallo
 // Test
 // Line3
