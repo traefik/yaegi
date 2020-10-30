29e7ef57177abb7cf2ad6e613be96ac874ccb845
Marc Vertes <mvertes@free.fr>
2020-10-30 15:39:43 +0100

    apply review remarks

--- _test/nil3.go
+++ _test/nil3.go
@@ -13,12 +13,16 @@ func (t *T) Hello() { println("Hello") }
 func main() {
 	t := &T{}
 	println(t.h != nil)
+	println(t.h == nil)
 	t.h = t
 	println(t.h != nil)
+	println(t.h == nil)
 	t.h.Hello()
 }
 
 // Output:
 // false
 // true
+// true
+// false
 // Hello
