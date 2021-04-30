3ca2ceeebe7290b13a9cc6a934d004bd8b50fd8c
Marc Vertes <mvertes@free.fr>
2021-04-30 09:50:15 +0200

    Update _test/map30.go
    Co-authored-by: mpl <mathieu.lonjaret@gmail.com>

--- _test/map30.go
+++ _test/map30.go
@@ -4,14 +4,19 @@ import "strings"
 
 func f(s string) string { return "hello " + s }
 
+func g(s string) string { return "hi " + s }
+
 var methods = map[string]func(string) string{"f": f}
 
 func main() {
-	methods["g"] = strings.ToUpper
+	methods["i"] = strings.ToUpper
+	methods["g"] = g
 	println(methods["f"]("test"))
 	println(methods["g"]("test"))
+	println(methods["i"]("test"))
 }
 
 // Output:
 // hello test
+// hi test
 // TEST
