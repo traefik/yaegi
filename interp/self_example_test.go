0ba64fc3184ee78a7492c10280427e31842a4d29
Dan Kortschak <dan@kortschak.io>
2021-05-10 21:36:09 +0930

    interp: add example to show use of Self
    This example clarifies how `interp.Self` can be accessed.
    
    Please take a look.

---
+++ interp/self_example_test.go
@@ -0,0 +1,45 @@
+package interp_test
+
+import (
+	"log"
+
+	"github.com/traefik/yaegi/interp"
+	"github.com/traefik/yaegi/stdlib"
+)
+
+func ExampleInterpreter_self() {
+	i := interp.New(interp.Options{})
+
+	i.Use(stdlib.Symbols)
+	i.Use(interp.Symbols)
+
+	_, err := i.Eval(`import (
+	"fmt"
+	"log"
+
+	// Import interp to gain access to Self.
+	"github.com/traefik/yaegi/interp"
+)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = i.Eval(`
+		// Evaluate code directly.
+		fmt.Println("Hello Yaegi from Go")
+
+		// Evaluate code indirectly via the Self access point.
+		_, err := interp.Self.Eval("fmt.Println(\"Hello Yaegi from Yaegi\")")
+		if err != nil {
+			log.Fatal(err)
+		}
+`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Output:
+	//
+	// Hello Yaegi from Go
+	// Hello Yaegi from Yaegi
+}
