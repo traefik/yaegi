traefik/yaegi

commit d16bd4bcdb8685f7ce88a0ee2261f7d945a6cc84

interp: fix package name extraction from import path
The heuristic to generate a package name identifier was incorrect. Now for binary packages, the package identifier is obtained by a symbol, generated by extract, which contains the string argument of package statement in source file. This should ensure an always correct default package identifier. 

Fixes #1095.

stdlib/unrestricted/unrestricted.go
@@ -35,7 +35,7 @@ func init() {
 		"Logger":  reflect.ValueOf((*log.Logger)(nil)),
 	}
 
-	Symbols["github.com/traefik/yaegi/stdlib/unrestricted"] = map[string]reflect.Value{
+	Symbols["github.com/traefik/yaegi/stdlib/unrestricted/unrestricted"] = map[string]reflect.Value{
 		"Symbols": reflect.ValueOf(Symbols),
 	}
 }

commit 2bdfb6e6d54eb65ec46210551fa755ef319e5633

interp: fix use of unrestricted symbols
This is fixing a regression introduced by #1102.

Fixes #1129.

stdlib/unrestricted/unrestricted.go
@@ -12,12 +12,12 @@ import (
 var Symbols = map[string]map[string]reflect.Value{}
 
 func init() {
-	Symbols["os"] = map[string]reflect.Value{
+	Symbols["os/os"] = map[string]reflect.Value{
 		"Exit":        reflect.ValueOf(os.Exit),
 		"FindProcess": reflect.ValueOf(os.FindProcess),
 	}
 
-	Symbols["os/exec"] = map[string]reflect.Value{
+	Symbols["os/exec/exec"] = map[string]reflect.Value{
 		"Command":        reflect.ValueOf(exec.Command),
 		"CommandContext": reflect.ValueOf(exec.CommandContext),
 		"ErrNotFound":    reflect.ValueOf(&exec.ErrNotFound).Elem(),
@@ -27,7 +27,7 @@ func init() {
 		"ExitError":      reflect.ValueOf((*exec.ExitError)(nil)),
 	}
 
-	Symbols["log"] = map[string]reflect.Value{
+	Symbols["log/log"] = map[string]reflect.Value{
 		"Fatal":   reflect.ValueOf(log.Fatal),
 		"Fatalf":  reflect.ValueOf(log.Fatalf),
 		"Fatalln": reflect.ValueOf(log.Fatalln),