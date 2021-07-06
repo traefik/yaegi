c74fb80dd61169b5fd464e927d9f435b9faf2d6b
Marc Vertes <mvertes@free.fr>
2021-07-06 11:54:00 +0200

    Update interp/cfg.go
    Co-authored-by: mpl <mathieu.lonjaret@gmail.com>

--- interp/cfg.go
+++ interp/cfg.go
@@ -1512,7 +1512,7 @@ func (interp *Interpreter) cfg(root *node, importPath string) ([]*node, error) {
 				// Find a matching field.
 				if ti := n.typ.lookupField(n.child[1].ident); len(ti) > 0 {
 					if isStruct(n.typ) {
-						// If a method of the same name exists, use it if it is less shallowed.
+						// If a method of the same name exists, use it if it is shallower than the struct field.
 						// if method's depth is the same as field's, this is an error.
 						d := n.typ.methodDepth(n.child[1].ident)
 						if d >= 0 && d < len(ti) {
