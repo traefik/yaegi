ff0e910ac6fd373a4faaff02d4e77a1d30f1b8e0
Ethan Reesor <ethan.reesor@gmail.com>
2021-07-16 22:36:53 -0500

    DAP types

---
+++ internal/cmd/json_schema/overrides.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+)
+
+type Schema_Type []SimpleTypes
+
+func (t *Schema_Type) UnmarshalJSON(b []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+
+	switch v := v.(type) {
+	case nil:
+		*t = nil
+	case string:
+		*t = Schema_Type{SimpleTypes(v)}
+	case []string:
+		*t = make(Schema_Type, len(v))
+		for i := range v {
+			(*t)[i] = SimpleTypes(v[i])
+		}
+	default:
+		return json.Unmarshal(b, (*[]SimpleTypes)(t))
+	}
+	return nil
+}
+
+func (t Schema_Type) MarshalJSON() ([]byte, error) {
+	if len(t) == 1 {
+		return json.Marshal(t[0])
+	}
+	return json.Marshal([]SimpleTypes(t))
+}
---
+++ internal/dap/generate.go
@@ -0,0 +1,3 @@
+package dap
+
+//go:generate go run ../cmd/json_schema --name dap --path types.go --url https://microsoft.github.io/debug-adapter-protocol/debugAdapterProtocol.json
