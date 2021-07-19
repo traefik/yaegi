c6e140e32a10080c1760ad9b5c268d3b8ef6c6a3
Marc Vertes <mvertes@free.fr>
2021-07-19 15:07:23 +0200

    cleanup comments

--- _test/interface52.go
+++ _test/interface52.go
@@ -1,20 +1,11 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
 
 func main() {
 	t := testing.T{}
 	var tb testing.TB
 	tb = &t
-	//tmpdir := os.Getenv("TMPDIR")
-	//if tmpdir == "" {
-	//	println("FAIL")
-	//	return
-	//}
-	// fmt.Fprintln(os.Stdout, "tmpdir:", tmpdir, "testing tmpdir:", tb.TempDir())
-	// if !strings.HasPrefix(tb.TempDir(), tmpdir) {
 	if tb.TempDir() == "" {
 		println("FAIL")
 		return
