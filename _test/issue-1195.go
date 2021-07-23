29b68c85596741adb2e7f215cbd515b259b95609
Marc Vertes <mvertes@free.fr>
2021-07-23 11:01:34 +0200

    stdlib: add missing wrappers of io/fs for go1.16
    Fixes #1195

---
+++ _test/issue-1195.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"errors"
+	"io/fs"
+)
+
+func main() {
+	pe := fs.PathError{}
+	pe.Op = "nothing"
+	pe.Path = "/nowhere"
+	pe.Err = errors.New("an error")
+	println(pe.Error())
+}
+
+// Output:
+// nothing /nowhere: an error
