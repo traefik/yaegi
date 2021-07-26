c80c605ab99e779cc9e6e37f00d0ddbdbc107c5e
Marc Vertes <mvertes@free.fr>
2021-07-26 19:12:11 +0200

    interp: support wrapping of nested interfaces
    Add getConcreteType to retrieve the concrete type of a nested interface
    value implementing a specific interface for which a wrapper exists.
    
    If method resolution fails at runtime, a panic is now issued instead
    of an error message and continue.
    
    Fixes #1187.

---
+++ _test/issue-1187.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+type sink interface {
+	io.Writer
+	io.Closer
+}
+
+func newSink() sink {
+	// return os.Stdout	// Stdout is special in yaegi tests
+	file, err := ioutil.TempFile("", "yaegi-test.*")
+	if err != nil {
+		panic(err)
+	}
+	return file
+}
+
+func main() {
+	s := newSink()
+	n, err := s.Write([]byte("Hello\n"))
+	if err != nil {
+		panic(err)
+	}
+	var writer io.Writer = s
+	m, err := writer.Write([]byte("Hello\n"))
+	if err != nil {
+		panic(err)
+	}
+	var closer io.Closer = s
+	err = closer.Close()
+	if err != nil {
+		panic(err)
+	}
+	err = os.Remove(s.(*os.File).Name())
+	if err != nil {
+		panic(err)
+	}
+	println(m, n)
+}
+
+// Output:
+// 6 6
