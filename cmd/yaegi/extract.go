367bad0090715ea8eb975315ee2c080f3e05db1d
Bai Yingjie <byj.tea@gmail.com>
2021-09-04 18:11:00 +0000

    extract: add an option to be able to set build tag to the created file

--- cmd/yaegi/extract.go
+++ cmd/yaegi/extract.go
@@ -18,12 +18,14 @@ func extractCmd(arg []string) error {
 	var name string
 	var exclude string
 	var include string
+	var tag string
 
 	eflag := flag.NewFlagSet("run", flag.ContinueOnError)
 	eflag.StringVar(&licensePath, "license", "", "path to a LICENSE file")
 	eflag.StringVar(&name, "name", "", "the namespace for the extracted symbols")
 	eflag.StringVar(&exclude, "exclude", "", "comma separated list of regexp matching symbols to exclude")
 	eflag.StringVar(&include, "include", "", "comma separated list of regexp matching symbols to include")
+	eflag.StringVar(&tag, "tag", "", "build tag to be added to the created package")
 	eflag.Usage = func() {
 		fmt.Println("Usage: yaegi extract [options] packages...")
 		fmt.Println("Options:")
@@ -55,6 +57,7 @@ func extractCmd(arg []string) error {
 	ext := extract.Extractor{
 		Dest:    name,
 		License: license,
+		Tag:     tag,
 	}
 
 	if exclude != "" {
