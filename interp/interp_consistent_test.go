30ea9fc607b4e05400481cfccea414a0d4bfab42
Marc Vertes <mvertes@free.fr>
2022-03-22 10:02:23 +0100

    add comment

--- interp/interp_consistent_test.go
+++ interp/interp_consistent_test.go
@@ -182,6 +182,9 @@ func TestInterpConsistencyBuild(t *testing.T) {
 
 func TestInterpErrorConsistency(t *testing.T) {
 	if runtime.Version() == "go1.18" {
+		// Error messages have changed a lot between go1.17 and go1.18.
+		// Skip testing on go1.18 while we support go1.17. It can be
+		// re-enabled after dropping go1.17.
 		t.Skip("skip go1.18")
 	}
 	testCases := []struct {
