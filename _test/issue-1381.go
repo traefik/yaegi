e9f30526cbb7befe1984def5deac3a8fcf6f32b1
mpl <mathieu.lonjaret@gmail.com>
2022-05-05 16:39:47 +0200

    review: rework a bit the test

--- _test/issue-1381.go
+++ _test/issue-1381.go
@@ -8,43 +8,51 @@ import (
 func main() {
 	var bufPtrOne *bytes.Buffer
 	var bufPtrTwo *bytes.Buffer
+	var bufPtrThree *bytes.Buffer
+	var bufPtrFour *bytes.Buffer
 
 	for i := 0; i < 2; i++ {
 		bufOne := bytes.Buffer{}
 		bufTwo := &bytes.Buffer{}
+		var bufThree bytes.Buffer
+		bufFour := new(bytes.Buffer)
 
 		if bufPtrOne == nil {
 			bufPtrOne = &bufOne
 		} else if bufPtrOne == &bufOne {
-			fmt.Println("KO")
+			fmt.Println("bufOne was not properly redeclared")
+		} else {
+			fmt.Println("bufOne is properly redeclared")
 		}
+
 		if bufPtrTwo == nil {
 			bufPtrTwo = bufTwo
 		} else if bufPtrTwo == bufTwo {
-			fmt.Println("KO")
+			fmt.Println("bufTwo was not properly redeclared")
+		} else {
+			fmt.Println("bufTwo is properly redeclared")
 		}
-	}
 
-	bufPtrOne = nil
-	bufPtrTwo = nil
-
-	for i := 0; i < 2; i++ {
-		var bufOne bytes.Buffer
-		bufTwo := new(bytes.Buffer)
-
-		if bufPtrOne == nil {
-			bufPtrOne = &bufOne
-		} else if bufPtrOne != &bufOne {
-			fmt.Println("OK")
+		if bufPtrThree == nil {
+			bufPtrThree = &bufThree
+		} else if bufPtrThree == &bufThree {
+			fmt.Println("bufThree was not properly redeclared")
+		} else {
+			fmt.Println("bufThree is properly redeclared")
 		}
-		if bufPtrTwo == nil {
-			bufPtrTwo = bufTwo
-		} else if bufPtrTwo != bufTwo {
-			fmt.Println("OK")
+
+		if bufPtrFour == nil {
+			bufPtrFour = bufFour
+		} else if bufPtrFour == bufFour {
+			fmt.Println("bufFour was not properly redeclared")
+		} else {
+			fmt.Println("bufFour is properly redeclared")
 		}
 	}
 }
 
 // Output:
-// OK
-// OK
+// bufOne is properly redeclared
+// bufTwo is properly redeclared
+// bufThree is properly redeclared
+// bufFour is properly redeclared
