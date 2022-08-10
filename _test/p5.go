b2aa636ea0d6a66d933511694072a2b624c0d6e3
Marc Vertes <mvertes@free.fr>
2022-08-10 16:10:08 +0200

    interp: fix spurious variable declaration loop
    The variable dependency check function was confused by a dependency
    variable with the same name but in an external package.
    
    This change is necessary to address #1427.

---
+++ _test/p5.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/traefik/yaegi/_test/p5"
+
+func main() {
+	println(*p5.Value1)
+}
+
+// Output:
+// value1
---
+++ _test/p5/p5.go
@@ -0,0 +1,8 @@
+package p5
+
+import "github.com/traefik/yaegi/_test/p4"
+
+var (
+	Value1 = &val1
+	val1   = p4.Value1
+)
