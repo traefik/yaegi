c1cdec4ea03b63aac28f06439d746e89a149eefb
Marc Vertes <mvertes@free.fr>
2022-09-12 17:03:06 +0200

    Update interp/type.go
    Co-authored-by: mpl <mathieu.lonjaret@gmail.com>

--- interp/type.go
+++ interp/type.go
@@ -456,10 +456,6 @@ func nodeType2(interp *Interpreter, sc *scope, n *node, seen []*node) (t *itype,
 					length = int(v.Int())
 				case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
 					length = int(v.Uint())
-				case reflect.Float32, reflect.Float64:
-					length = int(v.Float())
-				case reflect.Complex64, reflect.Complex128:
-					length = int(real(v.Complex()))
 				default:
 					return nil, c0.cfgErrorf("non integer constant %v", v)
 				}
