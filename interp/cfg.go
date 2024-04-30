9ed60ffee13a66e036f1b0567ca59631d8aef60b
Larry Clapp <lmclapp68+github@gmail.com>
2024-04-30 13:04:04 -0400

    interp: allow assignment to exported variables
    If you `(*interp.Interpreter).Use` a variable, you should be able to assign to it.
    
    Fixes #1623

--- interp/cfg.go
+++ interp/cfg.go
@@ -654,7 +654,7 @@ func (interp *Interpreter) cfg(root *node, sc *scope, importPath, pkgName string
 				var sym *symbol
 				var level int
 
-				if dest.rval.IsValid() && isConstType(dest.typ) {
+				if dest.rval.IsValid() && !dest.rval.CanSet() && isConstType(dest.typ) {
 					err = n.cfgErrorf("cannot assign to %s (%s constant)", dest.rval, dest.typ.str)
 					break
 				}
