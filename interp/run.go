67fc535008579864dd2c105edad217bb24306833
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-07-09 18:45:26 -0700

    smaller comment

--- interp/run.go
+++ interp/run.go
@@ -996,11 +996,8 @@ func genFunctionWrapper(n *node) func(*frame) reflect.Value {
 	return func(f *frame) reflect.Value {
 		v := value(f)
 		if !isDefer && v.Kind() == reflect.Func {
-			// per #1634, if v is already a func, then don't re-wrap!  critically, the original wrapping
-			// clones the frame, whereas the one here (below) does _not_ clone the frame, so it doesn't
-			// generate the proper closure capture effects!
-			// this path is the same as genValueAsFunctionWrapper which is the path taken above if
-			// the value has an associated node, which happens when you do f := func() ..
+			// fixes #1634, if v is already a func, then don't re-wrap
+			// because original wrapping cloned the frame but this doesn't
 			return v
 		}
 
