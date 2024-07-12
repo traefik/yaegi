1c0025287bfb36b779fad582d8863d1dcceca6e6
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-07-12 01:36:54 -0700

    remove D3 test

--- interp/generic_test.go
+++ interp/generic_test.go
@@ -142,20 +142,3 @@ func main() {
 		t.Error(err)
 	}
 }
-
-func TestD3(t *testing.T) {
-	i := New(Options{})
-	_, err := i.Eval(`
-package main
-
-import "github.com/traefik/yaegi/_test/d2"
-
-func main() {
-	f := d2.F
-	f()
-}
-`)
-	if err != nil {
-		t.Error(err)
-	}
-}
